c5125f05ca40894fb77a0a9c6300612f27ba3807
Matthijs4004 <matthijs4004@gmail.com>
2022-01-07 00:58:31 +0100

    updated clickerv3.py

--- clickerv3.py
+++ clickerv3.py
@@ -2,11 +2,21 @@
 import tkinter as tk
 from tkinter import IntVar
 from tkinter.constants import NUMERIC
+from typing import Literal
 # 1 knop zorgt er voor dat het nummer in de label met 1 omhoog gaat
 # 1 knop zorgt er voor dat het nummer in de label met 1 omlaag gaat.
 # Als het nummer 0 is moet de achtergrond grijs zijn.
 # Als het nummer kleiner dan 0 is moet de achtergrond rood zijn.
 # Als het nummer groter dan 0 is moet de achtergrond groen zijn.
+LastPressed = True
+
+def Pressed():
+    if LastPressed == True:
+        Label.bind("<Double-Button-1>", UpTwo)
+        print("Double Up")
+    elif LastPressed == False:
+        Label.bind("<Double-Button-1>", DownTwo)
+        print("Double Down")
 
 def Background():
     if number.get() > 0:
@@ -23,28 +33,34 @@ def HoverLeave(e):
     Background()
 
 def Up():
-    global number
+    global number,LastPressed
     number.set(number.get() + 1)
     Label.configure(textvariable=number)
     Background()
+    LastPressed = True
+    Pressed()
 
 def UpTwo(e):
-    global number
+    global number,LastPressed
     number.set(number.get() * 3)
     Label.configure(textvariable=number)
     Background()
+    LastPressed = False
 
 def DownTwo(e):
-    global number
+    global number,LastPressed
     number.set(number.get() / 3)
     Label.configure(textvariable=number)
     Background()
+    LastPressed = True
 
 def Down():
-    global number
+    global number,LastPressed
     number.set(number.get() - 1)
     Label.configure(textvariable=number)
     Background()
+    LastPressed = False
+    Pressed()
 
 window = tk.Tk()
 window.title("Clicker v3")
@@ -57,8 +73,12 @@ Label = tk.Label(bg="white",width=20, textvariable=number)
 buttonDown = tk.Button(bg="white",width=20, text="Down", bd=0, command=Down)
 Label.bind("<Enter>", Hover)
 Label.bind("<Leave>", HoverLeave)
-buttonUp.bind("<Double-Button-1>", UpTwo)
-buttonDown.bind("<Double-Button-1>", DownTwo)
+if LastPressed == True:
+    Label.bind("<Double-Button-1>", UpTwo)
+    print("Double Up")
+elif LastPressed == False:
+    Label.bind("<Double-Button-1>", DownTwo)
+    print("Double Down")
 buttonUp.place(y=45,x=50)
 Label.place(y=100,x=50)
 buttonDown.place(y=150,x=50)
